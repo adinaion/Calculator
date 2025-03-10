0e9e78e8772af8db13ee1e36a11320f86f40e521
adinaion <79267944+adinaion@users.noreply.github.com>
2025-03-10 13:32:58 +0200

    Initial Setup

---
+++ Calculator/Calculator/Model/CalculatorModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorModel
+    {
+        public double CurrentValue { get; private set; } = 0;
+
+        // Apelată pentru fiecare operație
+        public void PerformOperation(string operation, double operand)
+        {
+            switch (operation)
+            {
+                case "+":
+                    CurrentValue += operand;
+                    break;
+                case "-":
+                    CurrentValue -= operand;
+                    break;
+                case "*":
+                    CurrentValue *= operand;
+                    break;
+                case "/":
+                    if (operand != 0)
+                        CurrentValue /= operand;
+                    else
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                    break;
+                case "%":
+                    CurrentValue = CurrentValue % operand;
+                    break;
+                case "x^2":
+                    CurrentValue = Math.Pow(CurrentValue, 2);
+                    break;
+                case "sqrt":
+                    if (CurrentValue < 0)
+                        throw new InvalidOperationException("Cannot take the square root of a negative number.");
+                    CurrentValue = Math.Sqrt(CurrentValue);
+                    break;
+                case "+/-":
+                    CurrentValue = -CurrentValue;
+                    break;
+                case "1/x":
+                    if (CurrentValue == 0)
+                        throw new InvalidOperationException("Cannot divide by zero.");
+                    CurrentValue = 1 / CurrentValue;
+                    break;
+                default:
+                    throw new InvalidOperationException("Unknown operation.");
+            }
+        }
+
+        // Resetare la zero
+        public void Clear()
+        {
+            CurrentValue = 0;
+        }
+    }
+}
---
+++ Calculator/Calculator/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Calculator.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel
+{
+    public class CalculatorViewModel
+    {
+        private CalculatorModel _calculatorModel;
+
+        public CalculatorViewModel()
+        {
+            _calculatorModel = new CalculatorModel();
+        }
+
+        // Display-ul care va fi legat în XAML
+        public string Display => _calculatorModel.CurrentValue.ToString();
+
+        #region Commands
+
+        public ICommand AddCommand => new RelayCommand(param => ExecuteOperation("+", param));
+        public ICommand SubtractCommand => new RelayCommand(param => ExecuteOperation("-", param));
+        public ICommand MultiplyCommand => new RelayCommand(param => ExecuteOperation("*", param));
+        public ICommand DivideCommand => new RelayCommand(param => ExecuteOperation("/", param));
+        public ICommand ModulusCommand => new RelayCommand(param => ExecuteOperation("%", param));
+        public ICommand SquareCommand => new RelayCommand(param => ExecuteOperation("x^2", param));
+        public ICommand SquareRootCommand => new RelayCommand(param => ExecuteOperation("sqrt", param));
+        public ICommand NegateCommand => new RelayCommand(param => ExecuteOperation("+/-", param));
+        public ICommand InverseCommand => new RelayCommand(param => ExecuteOperation("1/x", param));
+        public ICommand ClearCommand => new RelayCommand(param => Clear());
+
+        #endregion
+
+        private void ExecuteOperation(string operation, object parameter)
+        {
+            try
+            {
+                double operand = 0;
+
+                // Verificăm dacă butonul apăsat este un număr
+                if (parameter != null && double.TryParse(parameter.ToString(), out operand))
+                {
+                    _calculatorModel.PerformOperation(operation, operand);
+                }
+                else
+                {
+                    _calculatorModel.PerformOperation(operation, 0); // Dacă nu avem operand, executăm fără valoare
+                }
+
+                // Actualizarea display-ului
+                OnPropertyChanged(nameof(Display));
+            }
+            catch (Exception ex)
+            {
+                // Tratează erorile (ex: diviziune la 0)
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Clear()
+        {
+            _calculatorModel.Clear();
+            OnPropertyChanged(nameof(Display));
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
